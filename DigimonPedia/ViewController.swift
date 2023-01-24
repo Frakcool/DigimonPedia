7dbdbbb6d8ea225d8fb58cf48a1d88d5fc3aea43
Jesus Sanchez <jesussa@microsoft.com>
2023-01-23 20:42:44 -0600

    Initial Commit

---
+++ DigimonPedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DigimonPedia
+//
+//  Created by Jesus Sanchez on 23/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
